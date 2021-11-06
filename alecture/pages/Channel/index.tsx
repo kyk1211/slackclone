edc4d17f7c31097b87b058e7429547cf07301ede
곽윤구 <dbsrndi123@naver.com>
2021-11-06 15:32:01 +0900

    make DMpage chatbox

--- alecture/pages/Channel/index.tsx
+++ alecture/pages/Channel/index.tsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import ChatBox from '@components/ChatBox';
+import ChatList from '@components/ChatList';
+import useInput from '@hooks/useInput';
+import React, { useCallback } from 'react';
 import { Container, Header } from './styles';
 
 const Channel = () => {
+  const [chat, onChangeChat, setChat] = useInput('');
+
+  const onSubmitForm = useCallback((e) => {
+    e.preventDefault();
+    setChat('');
+  }, []);
+
   return (
     <Container>
       <Header>channel</Header>
+      <ChatList />
+      <ChatBox chat={chat} onChangeChat={onChangeChat} onSubmitForm={onSubmitForm} />
     </Container>
   );
 };
--- alecture/pages/DirectMessage/index.tsx
+++ alecture/pages/DirectMessage/index.tsx
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { Container, Header } from './styles';
 import gravartar from 'gravatar'
 import useSWR from 'swr';
 import fetcher from '@utils/fetcher';
 import { useParams } from 'react-router';
+import ChatBox from '@components/ChatBox';
+import ChatList from '@components/ChatList';
+import useInput from '@hooks/useInput';
 
 const DirectMessage = () => {
   const { workspace, id } = useParams<{ workspace: string, id: string }>();
   const { data: userData } = useSWR(`/api/workspaces/${workspace}/users/${id}`, fetcher,);
   const { data: myData } = useSWR(`/api/users`, fetcher,);
+  const [chat, onChangeChat, setChat] = useInput('');
+
+  const onSubmitForm = useCallback((e) => {
+    e.preventDefault();
+    setChat('');
+  }, []);
 
   if (!userData || !myData) {
     return null;
@@ -20,8 +29,8 @@ const DirectMessage = () => {
         <img src={gravartar.url(userData.email, { s: '24px', d: 'retro' })} alt={userData.nickname} />
         <span>{userData.nickname}</span>
       </Header>
-      {/* <ChatList />
-      <ChatBox /> */}
+      <ChatList />
+      <ChatBox chat={chat} onChangeChat={onChangeChat} onSubmitForm={onSubmitForm} />
     </Container>
   );
 };

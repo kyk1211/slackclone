61d3fe60175741db6793983faa4070c0cbd7c5d3
곽윤구 <dbsrndi123@naver.com>
2021-11-11 20:03:13 +0900

    view DM Chat

--- alecture/pages/DirectMessage/index.tsx
+++ alecture/pages/DirectMessage/index.tsx
@@ -1,6 +1,6 @@
 import React, { useCallback } from 'react';
 import { Container, Header } from './styles';
-import gravartar from 'gravatar'
+import gravartar from 'gravatar';
 import useSWR from 'swr';
 import fetcher from '@utils/fetcher';
 import { useParams } from 'react-router';
@@ -11,28 +11,33 @@ import axios from 'axios';
 import { IDM } from '@typings/db';
 
 const DirectMessage = () => {
-  const { workspace, id } = useParams<{ workspace: string, id: string }>();
-  const { data: userData } = useSWR(`/api/workspaces/${workspace}/users/${id}`, fetcher,);
-  const { data: myData } = useSWR(`/api/users`, fetcher,);
+  const { workspace, id } = useParams<{ workspace: string; id: string }>();
+  const { data: userData } = useSWR(`/api/workspaces/${workspace}/users/${id}`, fetcher);
+  const { data: myData } = useSWR(`/api/users`, fetcher);
   const [chat, onChangeChat, setChat] = useInput('');
   const { data: chatData, mutate: mutateChat } = useSWR<IDM[]>(
-    `/api/workspaces/${workspace}/dms/${id}/chats>perPage=20/&page=1`, fetcher);
+    `/api/workspaces/${workspace}/dms/${id}/chats?perPage=20&page=1`,
+    fetcher,
+  );
 
-  const onSubmitForm = useCallback((e) => {
-    e.preventDefault();
-    console.log(chat);
-    if (chat?.trim() && chatData) {
-      axios.post(`/api/workspaces/${workspace}/dms/${id}/chats`, {
-        content: chat,
-      })
-        .then(() => {
-          mutateChat();
-          setChat('');
-        })
-        .catch(console.error)
-    }
-
-  }, [chat, workspace, id, chatData, mutateChat, setChat]);
+  const onSubmitForm = useCallback(
+    (e) => {
+      e.preventDefault();
+      console.log(chat);
+      if (chat?.trim() && chatData) {
+        axios
+          .post(`/api/workspaces/${workspace}/dms/${id}/chats`, {
+            content: chat,
+          })
+          .then(() => {
+            mutateChat();
+            setChat('');
+          })
+          .catch(console.error);
+      }
+    },
+    [chat, workspace, id, chatData, mutateChat, setChat],
+  );
 
   if (!userData || !myData) {
     return null;
@@ -44,7 +49,7 @@ const DirectMessage = () => {
         <img src={gravartar.url(userData.email, { s: '24px', d: 'retro' })} alt={userData.nickname} />
         <span>{userData.nickname}</span>
       </Header>
-      <ChatList />
+      <ChatList chatData={chatData} />
       <ChatBox chat={chat} onChangeChat={onChangeChat} onSubmitForm={onSubmitForm} />
     </Container>
   );

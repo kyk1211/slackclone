576541355e3fc1e5b910413b2bd6ab9c42fa14d2
곽윤구 <dbsrndi123@naver.com>
2021-12-05 21:17:12 +0900

    reverse infinite scroll useSWRInfinite

--- alecture/pages/DirectMessage/index.tsx
+++ alecture/pages/DirectMessage/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useRef } from 'react';
 import { Container, Header } from './styles';
 import gravartar from 'gravatar';
 import useSWR from 'swr';
@@ -10,21 +10,28 @@ import useInput from '@hooks/useInput';
 import axios from 'axios';
 import { IDM } from '@typings/db';
 import makeSection from '@utils/makeSection';
+import Scrollbars from 'react-custom-scrollbars';
+import useSWRInfinite from 'swr/infinite';
 
 const DirectMessage = () => {
   const { workspace, id } = useParams<{ workspace: string; id: string }>();
   const { data: userData } = useSWR(`/api/workspaces/${workspace}/users/${id}`, fetcher);
   const { data: myData } = useSWR(`/api/users`, fetcher);
   const [chat, onChangeChat, setChat] = useInput('');
-  const { data: chatData, mutate: mutateChat } = useSWR<IDM[]>(
-    `/api/workspaces/${workspace}/dms/${id}/chats?perPage=20&page=1`,
+  const {
+    data: chatData,
+    mutate: mutateChat,
+    setSize,
+  } = useSWRInfinite<IDM[]>(
+    (index) => `/api/workspaces/${workspace}/dms/${id}/chats?perPage=20&page=${index + 1}`,
     fetcher,
   );
-
+  const isEmpty = chatData?.[0]?.length === 0;
+  const isReachingEnd = isEmpty || (chatData && chatData[chatData.length - 1]?.length < 20) || false;
+  const scrollbarRef = useRef<Scrollbars>(null);
   const onSubmitForm = useCallback(
     (e) => {
       e.preventDefault();
-      console.log(chat);
       if (chat?.trim() && chatData) {
         axios
           .post(`/api/workspaces/${workspace}/dms/${id}/chats`, {
@@ -44,7 +51,7 @@ const DirectMessage = () => {
     return null;
   }
 
-  const chatSections = makeSection(chatData ? [...chatData].reverse() : []);
+  const chatSections = makeSection(chatData ? chatData.flat().reverse() : []);
 
   return (
     <Container>
@@ -52,7 +59,13 @@ const DirectMessage = () => {
         <img src={gravartar.url(userData.email, { s: '24px', d: 'retro' })} alt={userData.nickname} />
         <span>{userData.nickname}</span>
       </Header>
-      <ChatList chatSections={chatSections} />
+      <ChatList
+        chatSections={chatSections}
+        ref={scrollbarRef}
+        setSize={setSize}
+        isEmpty={isEmpty}
+        isReachingEnd={isReachingEnd}
+      />
       <ChatBox chat={chat} onChangeChat={onChangeChat} onSubmitForm={onSubmitForm} />
     </Container>
   );
--- alecture/utils/fetcher.ts
+++ alecture/utils/fetcher.ts
@@ -1,8 +1,11 @@
-import axios from 'axios'
+import axios from 'axios';
 
-const fetcher = (url: string) => axios.get(url, {
-  // withCredentials : 캐시 관련
-  withCredentials: true
-}).then((res) => res.data);
+const fetcher = <Data>(url: string) =>
+  axios
+    .get<Data>(url, {
+      // withCredentials : 캐시 관련
+      withCredentials: true,
+    })
+    .then((res) => res.data);
 
 export default fetcher;
